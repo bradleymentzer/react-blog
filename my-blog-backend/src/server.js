202d86cafa38e7d2c99e7931defc14c9a3df35f9
Bradley Mentzer <bradleymentzer@gmail.com>
2022-05-14 08:20:20 -0400

    Add upvote functionality to articles page

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const app = express();
+//a fake database placeholder before using
+const articlesInfo = {
+  "react-blog": {
+    upvotes: 0,
+  },
+  "node.js": {
+    upvotes: 0,
+  },
+};
 
 //parses json object included with post request and adds body property to request parameter of matching route
 app.use(bodyParser.json());
@@ -9,11 +17,20 @@ app.use(bodyParser.json());
 //recieves a get request on the endpoint /hello and repospons with a responce hello.
 //information contained in URL
 // Takes a callback argument which contains details about the request(req) and a reponce object(res)
-app.get(`/hello`, (req, res) => res.send(`Hello`));
-app.get(`/hello/:name`, (req, res) => res.send(`Hello ${req.params.name}`));
+// app.get(`/hello`, (req, res) => res.send(`Hello`));
+// app.get(`/hello/:name`, (req, res) => res.send(`Hello ${req.params.name}`));
 
 //post requets carry additional inpormation to pass along data to the server
-app.post(`/hello`, (req, res) => res.send(`Hello ${req.body.name}`));
+// app.post(`/hello`, (req, res) => res.send(`Hello ${req.body.name}`));
+
+app.post(`/api/articles/:name/upvote`, (req, res) => {
+  const articleName = req.params.name;
+
+  //add upvote to database
+  articlesInfo[articleName].upvotes += 1;
+  //how many upvotes the article has
+  res.status(200).send(`${articleName} now has ${articlesInfo} upvotes`);
+});
 
 //start server, takes a port to start and a callback to get called when server is listening, this logs message
 app.listen(8000, () => console.log(`Listening on port 8000`));
