a30a60e0552fc1b7dc61efab3143984fbff2579c
Bradley Mentzer <bradleymentzer@gmail.com>
2022-05-14 10:52:16 -0400

    Add fetch data from api

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArticlesList from "../components/ArticlesList";
 import articleContent from "./article-content";
 
@@ -6,6 +6,22 @@ const ArticlePage = ({ match }) => {
   const name = match.params.name;
   //finds article in article content and displays by name
   const article = articleContent.find((article) => article.name === name);
+
+  //define articleInfo which is populated by sending a request to the server
+  //setArticleInfo changes value of article info
+  //useState is initial value of articleInfo before we loaded value or changed state
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  //updates setArticleInfo every time URL changes
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`); //hold result of fetch
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+
   if (!article) return <h1>Article does not exist!</h1>;
 
   //does not display current article in other articles section
@@ -16,6 +32,7 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has been upvoted {articleInfo.upvotes} times.</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
